7a08637b551c1f7c9d3be9394db46e4c89e4329b
Ian Chen <ian.2780889@hotmail.com>
2021-03-21 16:30:52 +1300

    PseudoCode

---
+++ rock-paper-scissors.js
@@ -0,0 +1,11 @@
+/* ComputerPlay Function creates random number from ranging from 1-3 and accordingly spits out corresponding RPS
+    PlayerPlay Function lists possible options and user types out the RPS they want to use 
+    RPS function takes two parameters ComputerSelection PlayerSelection
+        If ComputerSelection and PlayerSelection is the same then tie 
+        If ComputerSelection = Paper and PlayerSelection = Rock , Computer win
+        If ComputerSelection = Scissors and PlayerSelection = Paper, Computer win 
+        If ComputerSelection = Rock and PlayerSelection = Scissors, Computer win 
+        Else PlayerSelection wins
+    Game Function Displays winner and loser and keeps count of score 
+
+*/
